3b4a47d3de1c9fe6371a9fd09b74c645467119d2
Kantt-debug <nikitok010406@gmail.com>
2026-01-19 22:52:20 +0500

    главное меню
    ну по названия понятно

---
+++ scene/settings_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var volume_slider = $ColorRect/VBoxContainer/HSlider
+@onready var resolution_box = $ColorRect/VBoxContainer/OptionButton
+
+func _ready():
+	# Заполняем список разрешений
+	resolution_box.add_item("Полноэкранный")
+	resolution_box.add_item("1280x720")
+	resolution_box.add_item("1920x1080")
+
+	# Стартовое значение громкости
+	volume_slider.value = AudioServer.get_bus_volume_db(0)
+
+func _on_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(0, value)
+
+func _on_resolution_box_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		1:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		2:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scene/main-menu.tscn")
